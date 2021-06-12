414a68dcc8f40289a4f6fa8d216d3df32912dc45
Rakin <rakinhstu18@gmail.com>
2021-06-12 20:30:59 +0600

    augmented arithmetic operations

---
+++ Example05.js
@@ -0,0 +1,18 @@
+// remainder
+var remainder = 22 % 3;
+
+// Augmented arithmetic operations
+
+var number = 8;
+
+// addition
+number += 10;   // number = 18;
+
+// subtraction
+number -= 5;    // number = 13;
+
+// multiplication
+number *= 10;   // number = 130;
+
+// division
+number /= 2;    // number = 65;
