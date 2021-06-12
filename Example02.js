6ee6281ac8bb8ff9d2bf2d05860454df485eb3b7
Rakin <rakinhstu18@gmail.com>
2021-06-12 08:29:22 +0600

    basic mathematical operations

---
+++ Example02.js
@@ -0,0 +1,15 @@
+// Operations on numbers
+var firstNumber = 10;
+var secondNumber = 2;
+
+// addition
+var result = firstNumber + secondNumber;
+
+// subtraction
+result = firstNumber - secondNumber;
+
+// multiplication
+result = firstNumber * secondNumber;
+
+// division
+result = firstNumber / secondNumber;
