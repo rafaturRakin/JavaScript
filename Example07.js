5fe5a2a03fccf0ffdc3406aba376f64e14a552e3
Rakin <rakinhstu18@gmail.com>
2021-06-13 17:09:49 +0600

    string operations

---
+++ Example07.js
@@ -0,0 +1,17 @@
+// string operations
+
+// concatenating
+var author = "Rakin";
+var myString = "First Part " + ", Second Part";
+myString += " and this is Third Part";
+myString = myString + ". Created by " + author;
+
+// string length
+var stringLength = myString.length;
+stringLength = "Rafatur".length;
+
+// indexing
+var firstLetter = author[0];    // firstLetter = R;
+var nthLetter = author[3];  // nthLetter = i;
+var lastLetter = author[author.length - 1]; // lastletter = n;
+var nthToLastLetter = author[author.length - 3];    // nthToLatLetter = k;
