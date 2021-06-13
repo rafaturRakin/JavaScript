76196daf04a46778b4317dd1cacf34fe2e7bf018
Rakin <rakinhstu18@gmail.com>
2021-06-13 16:37:38 +0600

    String and Escape sequence

---
+++ Example06.js
@@ -0,0 +1,17 @@
+// creating strings
+var doubleQuoteString = "Using double quote";
+var singleQuoteString = 'Using single quote';
+
+// using single and double quote
+doubleQuoteString = "It's awesome";
+singleQuoteString = 'He says, "Do it".';
+
+// escape sequence
+var myString = "For Single quote -> \' ";
+myString = "For Double quote -> \" ";
+myString = "For Backslash -> \\ ";
+myString = "For New line -> \n ";
+myString = "For Carriag return -> \r ";
+myString = "For Tab -> \t ";
+myString = "For Form feed -> \f ";
+myString = "For Word Boundary -> \b ";
